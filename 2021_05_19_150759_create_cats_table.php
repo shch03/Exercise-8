b8eb637505cfe0991d79ee9add199ea4246f71a3
Sheilla Chairiandita <66354489+shch03@users.noreply.github.com>
2021-05-24 13:18:24 +0800

    migrations

---
+++ 2021_05_19_150759_create_cats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->id('cat_id');
+            $table->string('name');
+            $table->date('date_of_birth');
+            $table->int('age');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cats');
+    }
+}
